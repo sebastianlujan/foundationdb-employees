c346a5ba5a3d82ff32b547fefd7405551d4c13c4
Mike McMahon <mmcm@comcast.net>
2013-05-15 16:42:28 -0400

    Switch to test without retry.

--- src/com/akiban/fdbemp/Loader.java
+++ src/com/akiban/fdbemp/Loader.java
@@ -57,41 +57,67 @@ public class Loader
 
     static final byte[] EMPTY = new byte[0];
     static final int COMMIT_COUNT = 100000;
+    static final boolean RETRY = true;
+
+    protected static class PendingSets implements Retryable {
+        private int count, limit;
+        private byte[][] pending;
+
+        public PendingSets(int limit) {
+            this.limit = limit;
+            pending = new byte[(limit+100)*2][];
+        }
+
+        public void empty() {
+            count = 0;
+        }
+
+        public void add(byte[] key, byte[] value) {
+            pending[count*2] = key;
+            pending[count*2+1] = value;
+            count++;
+        }
+
+        public boolean full() {
+            return (count >= limit);
+        }
+
+        @Override
+        public void attempt(Transaction tr) throws Exception {
+            for (int i = 0; i < count; i++) {
+                tr.set(pending[i*2], pending[i*2+1]);
+            }
+        }
+    }
 
     protected void load(LoadFile loadFile, Database db) throws Throwable {
         System.out.println("Loading " + loadFile.getName());
         try (FileReader r = new FileReader(loadFile.getFile())) {
             BufferedReader br = new BufferedReader(r);
             br.readLine();      // Header row.
-            final byte[][] pending = new byte[(COMMIT_COUNT+100)*2][];
-            int count = 0;
+            PendingSets pending = new PendingSets(COMMIT_COUNT);
             while (true) {
                 String line = br.readLine();
                 if (line != null) {
                     Object[] row = loadFile.parseRow(line);
                     Tuple hkey = Tuple.from(loadFile.hkey(row));
                     Tuple value = Tuple.from(row);
-                    pending[count*2] = hkey.pack();
-                    pending[count*2+1] = value.pack();
-                    count++;
+                    pending.add(hkey.pack(), value.pack());
                     for (LoadFile.Index index : loadFile.getIndexes()) {
                         Tuple key = Tuple.from(index.build(row));
-                        pending[count*2] = key.pack();
-                        pending[count*2+1] = EMPTY;
-                        count++;
+                        pending.add(key.pack(), EMPTY);
                     }
                 }
-                if ((line == null) || (count > COMMIT_COUNT)) {
-                    final int todo = count*2;
-                    db.run(new Retryable() {
-                            @Override
-                            public void attempt(Transaction tr) throws Exception {
-                                for (int i = 0; i < todo; i+=2) {
-                                    tr.set(pending[i], pending[i+1]);
-                                }
-                            }
-                        });
-                    count = 0;
+                if ((line == null) || pending.full()) {
+                    if (RETRY) {
+                        db.run(pending);
+                    }
+                    else {
+                        Transaction tr = db.createTransaction();
+                        pending.attempt(tr);
+                        tr.commit().get();
+                    }
+                    pending.empty();
                 }
                 if (line == null) break;
             }
